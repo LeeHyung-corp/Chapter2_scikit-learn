4c9b667721635f4010739ba4ada921ce914e6ffb
형주 이 <luke512@o365.yonsei.ac.kr>
2021-12-28 23:26:09 +0900

    20211228

--- main.py
+++ main.py
@@ -3,3 +3,15 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
 
 import pandas as pd
+
+# iris: 붓꽃 데이터 세트
+# iris_data: iris에서 피처(특징)만으로 된 numpy
+iris = load_iris()
+iris_data = iris.data
+iris_label = iris.target
+
+print('iris target값:', iris_label)
+print('iris target명:', iris.target_names)
+
+iris_df = pd.DataFrame(data=iris_data, columns=iris.feature_names)
+iris_df.head(3)
